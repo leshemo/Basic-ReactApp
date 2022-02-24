4f861645245d62880e2d38540f19aa66c0621f5f
leshemo <81444861+leshemo@users.noreply.github.com>
2022-02-24 18:03:40 -0500

    playing around

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
+import omriPic from './omriPic.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={omriPic} className="App-logo" alt="omriPic" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Testing out and playing around with React. Interesting stuff, though I definitely want to learn more. Maybe Vue.js next?
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
